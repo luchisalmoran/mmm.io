1fb43d9def2929c3827ceedc610196bec0cf7d8f
lucy <56303888+luchisalmoran@users.noreply.github.com>
2021-11-18 09:40:00 -0500

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,13 @@
+//common string methods
+
+let email = '122lsalmoran@frhds.com';
+
+// let result = email.lastindexOf('n')
+
+//let result = email.slice(2,5);
+
+//let result = email.substr(4,10);
+
+let result = email.replace('n','w');
+
+console.log(result);
